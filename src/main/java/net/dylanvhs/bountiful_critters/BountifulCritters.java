a04c270d18e7a9f3841c50ecdaa6f741bc2cf93b
Dylanvhs <dylanvhs257@gmail.com>
2024-04-01 15:09:30 +0200

    holy moly!

---
+++ src/main/java/net/dylanvhs/bountiful_critters/BountifulCritters.java
@@ -0,0 +1,59 @@
+package net.dylanvhs.bountiful_critters;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(BountifulCritters.MOD_ID)
+public class BountifulCritters
+{
+
+    public static final String MOD_ID = "bountiful_critters";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public BountifulCritters()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        ModEntities.register(modEventBus);
+
+        ModMenuTypes.register(modEventBus);
+
+        ModSounds.register(modEventBus);
+
+
+        modEventBus.addListener(this::commonSetup);
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
