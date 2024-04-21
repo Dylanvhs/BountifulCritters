330dfe1fd09a1fe60b2188a3f6786b7e8aed66b8
Dylanvhs <dylanvhs257@gmail.com>
2024-04-21 18:22:23 +0200

    lamp of salt partecl

--- src/main/java/net/dylanvhs/bountiful_critters/block/custom/SaltLampBlock.java
+++ src/main/java/net/dylanvhs/bountiful_critters/block/custom/SaltLampBlock.java
@@ -4,7 +4,9 @@ import javax.annotation.Nullable;
 
 import net.dylanvhs.bountiful_critters.item.ModItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -73,6 +75,27 @@ public class SaltLampBlock extends Block {
         }
 
     }
+    public void animateTick(BlockState p_222503_, Level p_222504_, BlockPos p_222505_, RandomSource p_222506_) {
+        if (p_222503_.getValue(LIT)) {
+            int i = p_222505_.getX();
+            int j = p_222505_.getY();
+            int k = p_222505_.getZ();
+            double d0 = (double) i + p_222506_.nextDouble();
+            double d1 = (double) j + 0.7D;
+            double d2 = (double) k + p_222506_.nextDouble();
+            p_222504_.addParticle(ParticleTypes.ELECTRIC_SPARK, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+            BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
+
+            for (int l = 0; l < 14; ++l) {
+                blockpos$mutableblockpos.set(i + Mth.nextInt(p_222506_, -10, 10), j - p_222506_.nextInt(10), k + Mth.nextInt(p_222506_, -10, 10));
+                BlockState blockstate = p_222504_.getBlockState(blockpos$mutableblockpos);
+                if (!blockstate.isCollisionShapeFullBlock(p_222504_, blockpos$mutableblockpos)) {
+                    p_222504_.addParticle(ParticleTypes.ELECTRIC_SPARK, (double) blockpos$mutableblockpos.getX() + p_222506_.nextDouble(), (double) blockpos$mutableblockpos.getY() + p_222506_.nextDouble(), (double) blockpos$mutableblockpos.getZ() + p_222506_.nextDouble(), 0.0D, 0.0D, 0.0D);
+                }
+            }
+        }
+    }
+
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(LIT);
