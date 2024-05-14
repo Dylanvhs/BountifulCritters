14d8d26f6c702dfca1fef85b63be2129bf1f5426
Dylanvhs <dylanvhs257@gmail.com>
2024-05-14 22:33:05 +0200

    fixes

--- src/main/java/net/dylanvhs/bountiful_critters/tags/ModTags.java
+++ src/main/java/net/dylanvhs/bountiful_critters/tags/ModTags.java
@@ -5,12 +5,15 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.levelgen.structure.Structure;
 
 public class ModTags {
+
+    public static final TagKey<PoiType> SNAKE_POT = registerPoiTypeTag("snake_pot");
 
 
 
@@ -32,6 +35,10 @@ public class ModTags {
 
     private static TagKey<Structure> registerStructureTag(String name) {
         return TagKey.create(Registries.STRUCTURE, new ResourceLocation(BountifulCritters.MOD_ID, name));
+    }
+
+    private static TagKey<PoiType> registerPoiTypeTag(String name) {
+        return TagKey.create(Registries.POINT_OF_INTEREST_TYPE, new ResourceLocation(BountifulCritters.MOD_ID, name));
     }
 }
 
