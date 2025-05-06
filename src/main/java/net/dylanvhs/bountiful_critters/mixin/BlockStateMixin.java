4231e38db83249c25682bd9cf4d35d7d143e465e
Dylanvhs <dylanvhs257@gmail.com>
2025-05-06 20:51:32 +0200

    attempt at fixing launch crash

--- src/main/java/net/dylanvhs/bountiful_critters/mixin/BlockStateMixin.java
+++ src/main/java/net/dylanvhs/bountiful_critters/mixin/BlockStateMixin.java
@@ -1,15 +1,15 @@
 package net.dylanvhs.bountiful_critters.mixin;
 
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Block.class)
-public abstract class BlockMixin {
+@Mixin(BlockBehaviour.BlockStateBase.class)
+public abstract class BlockStateMixin {
     @Inject(method = "isRandomlyTicking", at = @At("RETURN"), cancellable = true)
     public void tickPots(BlockState state, CallbackInfoReturnable<Boolean> cir) {
         if (state.is(Blocks.DECORATED_POT)) cir.setReturnValue(true);
